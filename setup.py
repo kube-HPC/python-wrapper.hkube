76ceed3ce042f07e8795fb9d52f0a5eb170f47f9
Nassi Harel <nassi9@gmail.com>
2020-03-09 11:26:30 +0200

    feat: setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-packages=setuptools.find_packages()
+packages = setuptools.find_packages()
 
 requires = [
     'Events>=0.3',
@@ -14,9 +14,9 @@ requires = [
     'simplejson',
     'gevent>=1.3.7',
     'pymongo>=3.10.1',
-    "wsaccel"
-
-
+    "wsaccel",
+    "dpath",
+    "six"
 ]
 
 with open("README.md", "r") as f:
@@ -27,20 +27,20 @@ with open(os.path.join(here, 'hkube_python_wrapper', '__version__.py'), 'r') as 
     exec(f.read(), about)
 
 setuptools.setup(
-     name=about['__title__'],  
-     version=about['__version__'],
-     author=about['__author__'],
-     author_email=about['__author_email__'],
-     description=about['__description__'],
-     license=about['__license__'],
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url=about['__url__'],
-     packages=packages,
-     install_requires=requires,
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
+    name=about['__title__'],
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    description=about['__description__'],
+    license=about['__license__'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=about['__url__'],
+    packages=packages,
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
