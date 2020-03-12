1e4a7829025b6bf448db38da7bc3e64dbb2f169f
golanha <golanha@rnd-hub.com>
2020-03-12 13:59:12 +0200

    zeroMQ 1

---
+++ communication/zmq/ZMQRequest.py
@@ -0,0 +1,21 @@
+
+import zmq
+
+
+
+context = zmq.Context()
+
+
+class ZMQRequest(object):
+    def __init__(self,reqDetails):
+        self.socket = context.socket(zmq.REQ)
+        self.socket.connect('tcp://'+reqDetails['host']+':'+str(reqDetails['port']))
+        self.connected = False
+        self.content = reqDetails['content']
+
+
+
+    def invoke(self):
+        self.socket.send(self.content)
+        message = self.socket.recv()
+        return message
