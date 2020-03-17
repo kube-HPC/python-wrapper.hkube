7ae08e81d1925cadfe0142a40ad8baecaf6da9ba
golanha <golanha@rnd-hub.com>
2020-03-17 10:09:22 +0200

    support gevent

--- communication/DataServer.py
+++ communication/DataServer.py
@@ -10,9 +10,28 @@ class DataServer:
         encoding = config['encoding']
         self.encoding = Encoding(encoding)
     def createReply(self,message):
-        decodedMessage = self.encoding.decode(message)
-        result = dpath.util.get(self.data,decodedMessage['dataPath'])
-        return  self.encoding.encode(result)
+        try:
+            decodedMessage = self.encoding.decode(message)
+            taskId = decodedMessage['taskId']
+            if(taskId != self.task):
+                result =  self.createError('notAvailable','Current taskId is '+ str(self.task))
+            else:
+                datapath = decodedMessage['dataPath']
+                if(datapath != None and len(str(datapath))>0):
+                    result ={'data': dpath.util.get( self.data,datapath)}
+                else:
+                    result = {'data':self.data}
+        except Exception as e:
+            result = self.createError('unknown',str(e))
+        finally:
+            return self.encoding.encode(result)
+
     def setSendingState(self,task,data):
         self.task = task
         self.data = data
+
+    def endSendingState(self):
+        self.task = None
+        self.data = None
+    def createError(self,code,message):
+        return {'error': {'code':code,'message':message}}
