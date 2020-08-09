c574cee3320872fd24520356907ca33817ec4667
golanha <golanha@rnd-hub.com>
2020-08-09 16:32:11 +0300

    pylint

--- hkube_python_wrapper/wrapper/algorunner.py
+++ hkube_python_wrapper/wrapper/algorunner.py
@@ -224,7 +224,7 @@ class Algorunner:
             messageListenrConfig, discovery, self._nodeName)
 
     def _start(self, options):
-        if (self._input.get('kind') == 'stream' and len(self._input['childs']) > 0):
+        if (self._input.get('kind') == 'stream' and self._input['childs']):
             def onStatistics(statistics):
                 self._sendCommand(
                     messages.outgoing.streamingStatistics, statistics)
