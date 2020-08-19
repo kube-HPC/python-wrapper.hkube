bff8499beab73b23d4fde83a91d4819c52c52290
golanha <golanha@rnd-hub.com>
2020-08-19 16:59:06 +0300

    pylint

--- hkube_python_wrapper/cache/caching.py
+++ hkube_python_wrapper/cache/caching.py
@@ -11,13 +11,13 @@ class CustomCache:
     def update(self, key, value):
         size = len(msgpack.packb(value))
         if (key in self._cache):
-            return
-        while (self.sumSize + size) >= self._maxCacheSize*1000*1000:
+            return None
+        while (self.sumSize + size) >= self._maxCacheSize * 1000 * 1000:
             if (len(self._cache.keys()) == 0):
-                print("Trying to insert a value of size " + str(size) + " bytes, larger than " + str(self._maxCacheSize) + "MB")
-                return
-            else:
-                self._remove_oldest()
+                print("Trying to insert a value of size " + str(size) + " bytes, larger than " + str(
+                    self._maxCacheSize) + "MB")
+                return None
+            self._remove_oldest()
         self._cache[key] = {'timestamp': datetime.datetime.now(), 'size': size, 'value': value}
         self.sumSize += size
         return key
@@ -32,7 +32,7 @@ class CustomCache:
                 oldest = key
             elif self._cache[key]['timestamp'] < self._cache[oldest]['timestamp']:
                 oldest = key
-        self.sumSize -= self._cache[key]['size']
+        self.sumSize -= self._cache[oldest]['size']
         self._cache.pop(oldest)
 
     def get(self, key):
