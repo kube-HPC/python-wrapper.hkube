88915e32906c7e55e330706018bbd41454e915d1
yehiyam <yehiyam@users.noreply.github.com>
2020-09-23 10:15:19 +0300

    Fix algorithm exit (#29)
    * fix exit
    
    * cleanup
    
    * fix tests
    
    * cleanup
    
    * fix default daemon

---
+++ hkube_python_wrapper/util/timerImpl.py
@@ -0,0 +1,28 @@
+import threading
+import sys
+if (sys.version_info > (3, 0)):
+    # Python 3 code in this block
+    # pylint: disable=inherit-non-class
+    class Timer(threading.Timer):
+        def __init__(self, interval, function, args=None, kwargs=None, name='', daemon=True):
+            # pylint: disable=non-parent-init-called
+            threading.Timer.__init__(self, interval, function, args, kwargs)
+            self.name = name
+            self.daemon = daemon
+else:
+    # Python 2 code in this block
+    class Timer():
+        def __init__(self, interval, function, args=[], kwargs={}, name='', daemon=True):
+            self.interval = interval
+            self.function = function
+            self.args = args
+            self.kwargs = kwargs
+            self.name = name
+            self.daemon = daemon
+            self._timer = None
+
+        def start(self):
+            self._timer = threading.Timer(self.interval, self.function, self.args, self.kwargs)
+            self._timer.name = self.name
+            self._timer.daemon = self.daemon
+            self._timer.start()
