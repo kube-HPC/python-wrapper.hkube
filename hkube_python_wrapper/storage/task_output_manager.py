57dbc6471830fa67325eda93a96f4335028c595e
Yehiyam Livneh <yehiyam@gmail.com>
2021-07-12 08:58:08 +0300

    lint

--- hkube_python_wrapper/storage/task_output_manager.py
+++ hkube_python_wrapper/storage/task_output_manager.py
@@ -15,7 +15,7 @@ class TaskOutputManager(BaseStorageManager):
     def get(self, jobId, taskId):
         return super(TaskOutputManager, self).get({'path': self.createPath(jobId, taskId)})
 
-    def list(self, jobId):
+    def list(self, jobId): # pylint: disable=arguments-renamed
         return super(TaskOutputManager, self).list({'path': self.createPath(jobId)})
 
     def delete(self, jobId, taskId=''):
