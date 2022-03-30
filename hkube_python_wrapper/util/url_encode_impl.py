4529718e4f20ab6d2afe92100b309541511d343a
Yehiyam Livneh <yehiyam@gmail.com>
2022-03-30 16:27:25 +0300

    fix lint

--- hkube_python_wrapper/util/url_encode_impl.py
+++ hkube_python_wrapper/util/url_encode_impl.py
@@ -1,10 +1,3 @@
-import sys
-if (sys.version_info > (3, 0)):
-    # Python 3 code in this block
-    from urllib.parse import quote_plus  # pylint: disable=import-error
-else:
-    # Python 2 code in this block
-    from urllib import quote_plus  # pylint: disable=import-error
-
+from six.moves import urllib
 def url_encode(url):
-    return quote_plus(url)
+    return urllib.parse.quote_plus(url)
