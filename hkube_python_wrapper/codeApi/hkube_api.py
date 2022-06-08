8ffb84acd9df11ce16472b6e8bf9e892a44ed1a6
golanha <golanha@rnd-hub.com>
2022-06-08 14:49:29 +0300

    get statistics mock as an array

--- hkube_python_wrapper/codeApi/hkube_api.py
+++ hkube_python_wrapper/codeApi/hkube_api.py
@@ -59,10 +59,10 @@ class HKubeApi:
         if (self.streamingManager.messageProducer is not None):
             return self.streamingManager.messageProducer.getStatistics()
         print("get_streaming_statistics returns no value in debug")
-        return {"nodeName": "NextNodeName",
+        return [{"nodeName": "NextNodeName",
                 "sent": -1,
                 "queueSize": -1,
-                "dropped": -1}
+                "dropped": -1}]
 
     def _generateExecId(self):
         self._lastExecId += 1
