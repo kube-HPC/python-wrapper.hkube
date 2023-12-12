7a8c855b98a35e0749dc9b51ec819138d2ab4756
golanha <g.olanlia@gmail.com>
2023-12-12 21:37:04 +0200

    multi fetch

--- hkube_python_wrapper/communication/streaming/StreamingListener.py
+++ hkube_python_wrapper/communication/streaming/StreamingListener.py
@@ -1,3 +1,4 @@
+import threading
 import time
 from hkube_python_wrapper.util.DaemonThread import DaemonThread
 
@@ -8,7 +9,8 @@ class StreamingListener(DaemonThread):
         self._working = True
         self._messageListeners = messageListeners
         DaemonThread.__init__(self, "StreamingListener")
-
+    def fetch(self,messageListener):
+        messageListener.fetch()
     def run(self):
         while (self._listeningToMessages):
             messageListeners = self._messageListeners()
@@ -16,7 +18,10 @@ class StreamingListener(DaemonThread):
                 time.sleep(1)  # free some cpu
                 continue
             for listener in messageListeners:
-                listener.fetch()
+                listener.thread = threading.Thread(target=self.fetch, args=[listener])
+                listener.thread.start()
+            for listener in messageListeners:
+                listener.thread.join()
         self._working = False
 
     def stop(self, force=True):
